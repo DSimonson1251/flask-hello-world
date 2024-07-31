e7ce453a4c50bbed8cee967bc1843b11b8aeb3e0
Daniel Simonson <dasi2816@colorado.edu>
2024-07-31 21:02:19 +0000

    Step 2 push

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World from Daniel Simonson in 3308"
+    return "Hello World from --your name-- in 3308"
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("your_db_url_here")
+    conn.close()
+    return "Database connection successful"
