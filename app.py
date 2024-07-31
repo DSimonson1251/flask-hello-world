2ae94767dba5d29530d2ba2187201049dfb6d069
Daniel Simonson <dasi2816@colorado.edu>
2024-07-31 21:21:08 +0000

    added create Db

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def db_test():
     conn = psycopg2.connect("postgresql://flaskhw3308_user:OS9wVypgEl9Za8I6Pejbn6WVVh3Z9hmT@dpg-cqlabo1u0jms73899qag-a/flaskhw3308")
     conn.close()
     return "Database connection successful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://flaskhw3308_user:OS9wVypgEl9Za8I6Pejbn6WVVh3Z9hmT@dpg-cqlabo1u0jms73899qag-a/flaskhw3308")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Create"
