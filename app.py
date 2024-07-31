259c4e0a6147693b1c7be5c9d8a370b24fec9cc2
Daniel Simonson <dasi2816@colorado.edu>
2024-07-31 21:10:14 +0000

    added db link

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello():
 
 @app.route('/db_test')
 def db_test():
-    conn = psycopg2.connect("your_db_url_here")
+    conn = psycopg2.connect("postgresql://flaskhw3308_user:OS9wVypgEl9Za8I6Pejbn6WVVh3Z9hmT@dpg-cqlabo1u0jms73899qag-a/flaskhw3308")
     conn.close()
     return "Database connection successful"
